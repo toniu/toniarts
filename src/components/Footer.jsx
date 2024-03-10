dfddf98100ae7ecef42eadcba53a6f329cbcd4a6
toniu <zhac032@live.rhul.ac.uk>
2024-03-10 01:32:57 +0000

    added new drawings.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+
+import { FaInstagram, FaPinterest } from "react-icons/fa";
+import { FaThreads } from "react-icons/fa6";
+
 /* Import for picture */
 import moi from '../assets/yute-artist.png'
 import backgroundImage from '../assets/wood-background.jpg'
@@ -7,10 +11,37 @@ const Footer = () => {
     return (
         <section className='h-screen flex justify-center items-center relative bg-black bg-opacity-60 '>
             <div className='container  bg-opacity-40 flex flex-col justify-center items-center space-y-7'>
-                <img className='w-[300px] h-[300px] object-cover rounded-lg hover:scale-110 cursor-pointer transition duration-100'
+                <img className='w-[250px] h-[250px] object-cover rounded-lg hover:scale-110 cursor-pointer transition duration-100'
                      src={moi} alt='me' />
                 <h3 className='py-2 text-white text-lg md:text-xl'>"a budding young artist in the nursery"</h3>
                 <h1 className='py-2 text-6xl md:text-6xl text-white'>fin.</h1>
+                <ul className='flex justify-center text-3xl text-white'>
+                    <li className='nav-item p-2'>
+                        <a href="https://www.instagram.com/toninekatu" target="_blank" rel="noopener noreferrer"
+                            className="mx-8
+                            hover:text-green-200 transition 100">
+                            <FaInstagram />
+                        </a>
+                    </li>
+                    <li className='nav-item p-2'>
+                        <a href="https://www.threads.net/toninekatu" target="_blank" rel="noopener noreferrer"
+                            className="mx-8
+                            hover:text-green-200 transition 100">
+                            <FaThreads/>
+                        </a>
+                    </li>
+                    <li className='nav-item p-2'>
+                        <a href="https://www.pinterest.co.uk/toniboards/drawingideas/" target="_blank" rel="noopener noreferrer"
+                            className="mx-8
+                            hover:text-green-200 transition 100">
+                            <FaPinterest/>
+                        </a>
+                    </li>
+                </ul>
+                <div className='bottom text-white text-center py-2'>
+                    <span className='line mb-4'></span>
+                    <p>2024 toni., inc. all rights reserved</p>
+                </div>
             </div>
             {/* Background Image */}
             <div className='absolute inset-0 z-[-1] bg-no-repeat bg-center bg-cover'
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -6,6 +6,10 @@ import P1 from '../assets/pic-1.jpg';
 import P2 from '../assets/pic-2.jpg';
 import P3 from '../assets/pic-3.jpg';
 import P4 from '../assets/pic-4.jpg';
+import P5 from '../assets/pic-5.jpg';
+import P6 from '../assets/pic-6.jpg';
+import P7 from '../assets/pic-7.jpg';
+import P8 from '../assets/pic-8.jpg';
 
 /* Real-life comparison images */
 import CP1 from '../assets/cp-1.jpg';
@@ -13,12 +17,21 @@ import CP2 from '../assets/cp-2.jpg';
 import CP3 from '../assets/cp-3.jpg';
 import CP4 from '../assets/cp-4.jpg';
 
+import CP5 from '../assets/cp-5.jpg';
+import CP6 from '../assets/cp-6.jpg';
+import CP7 from '../assets/cp-7.jpg';
+import CP8 from '../assets/cp-8.jpg';
+
 const Gallery = () => {
     const images = [
+        { title: 'iwobi.', imageUrl: P8, compareUrl: CP8 },
         { title: 'jozif.', imageUrl: P1, compareUrl: CP1 },
+        { title: 'memphis.', imageUrl: P5, compareUrl: CP5 },
         { title: 'adut.', imageUrl: P2, compareUrl: CP2 },
         { title: 'dre.', imageUrl: P3, compareUrl: CP3 },
+        { title: 'samuel.', imageUrl: P7, compareUrl: CP7 },
         { title: 'jabulani.', imageUrl: P4, compareUrl: CP4 },
+        { title: 'grace.', imageUrl: P6, compareUrl: CP6 },
     ];
 
     useEffect(() => {
@@ -69,7 +82,7 @@ const Gallery = () => {
                     backgroundSize: 'cover',
                     backgroundPosition: 'center',
                 }}>
-                <div className='scroll-section absolute h-full w-[500vw] will-change-transform px-[5vw] py-0 top-0 bg-gray-300/30 backdrop-blur-md'>
+                <div className='scroll-section absolute h-full w-[500vw] will-change-transform px-[5vw] py-0 top-0 bg-gray-300/30 backdrop-blur-[5px] '>
                     <div className='divs-container py-12 flex justify-items-stretch gap-x-[20px] md:gap-x-[40px]'>
                         {images.map((nextImage, index) => (
                             <div key={index}>
